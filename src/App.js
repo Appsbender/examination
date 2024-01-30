bffc57e4d48ec4591b8f737a2b5dde4eeb96c565
Appsbender <regz24@gmail.com>
2024-01-31 02:30:55 +0800

    Add login state only instead of using routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Management from './components/Management';
 import './css/styles.css';
 
 function App() {
+  const [isLoggedIn, setLoginStatus] = useState(false);
+  const handleLoginState = (status) => {
+    setLoginStatus(status);
+  }
+
+
   return (
-    <div className="apps-wrapper">
-        test
+    <div className={`apps-wrapper ${isLoggedIn ? 'overrides' : ''}`}>
+      {!isLoggedIn ? <Login isLoginState={handleLoginState} /> : ''}
+      {isLoggedIn ? <Management isLoginState={handleLoginState} /> : ''}
     </div>
   );
 }
